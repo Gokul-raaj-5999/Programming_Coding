e461842527c694319f8d49be029f4ce36b64f978
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-09-11 21:53:44 +0530

    chan

---
+++ My_personal_Coding/matrix_rotate_90.py
@@ -0,0 +1,26 @@
+def rotate(A):
+    N = len(A[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = A[i][j]
+            A[i][j] = A[N - 1 - j][i]
+            A[N - 1 - j][i] = A[N - 1 - i][N - 1 - j]
+            A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i]
+            A[j][N - 1 - i] = temp
+    return A
+     
+
+"""
+form
+A = [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+     [13, 14, 15, 16]]
+
+to
+ 
+13 9 5 1 
+14 10 6 2 
+15 11 7 3 
+16 12 8 4 
+"""
