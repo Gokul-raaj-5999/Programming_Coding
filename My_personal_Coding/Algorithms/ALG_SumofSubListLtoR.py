df799c6568f14a45cfb5318ab23df9afca64ed68
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-09-24 23:16:52 +0530

    alg

--- My_personal_Coding/Algorithms/ALG_SumofSubListLtoR.py
+++ My_personal_Coding/Algorithms/ALG_SumofSubListLtoR.py
@@ -1,14 +1,13 @@
-"""for this u have a array of n len 
-eg arr = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14]
-in this case u r given with the lower and upper length u have to find the sum of 
-elements that of that len so i this case u use this kind of algrothums 
-arr = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14] to find its sum and add it to a new list b[-1]
-so it will become 
-b = [1 3 6 10 15 21 28 36 45 55 66 72 85 99]
-and now what we do we subract b[r] - b[l-1] 
-if l == 0 then we make this b[r] thats all.
+# for this u have a array of n len 
+# eg arr = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14]
+# in this case u r given with the lower and upper length u have to find the sum of 
+# elements that of that len so i this case u use this kind of algrothums 
+# arr = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14] to find its sum and add it to a new list b[-1]
+# so it will become 
+# b = [1 3 6 10 15 21 28 36 45 55 66 72 85 99]
+# and now what we do we subract b[r] - b[l-1] 
+# if l == 0 then we make this b[r] thats all.
 
-for example"""
 
 n = int(input())
 v = list(map(int, input().split()))
