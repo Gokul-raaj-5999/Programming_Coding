9e80d86d285678ad3bb5bd1c2649855f17448ca1
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-09-24 23:06:03 +0530

    alg

--- My_personal_Coding/Algorithms/ALG_DP_Kadane's_MaxsumofSubarr, LR.py
+++ My_personal_Coding/Algorithms/ALG_DP_Kadane's_MaxsumofSubarr, LR.py
@@ -2,24 +2,29 @@
 #changes for element to element.. so in that case we have a totsum = X
 # we have to find a sum that is >= totsum that is our aim
 # sumfind(from subarray) >= total_sum.
-
-maxint = 99999999999
+from sys import maxsize
 
 def maxSubArraySum(a, size):
+	maxsum = 0
+	cursum = 0
+	maxval = -maxsize -1
+	minval = maxsize
 
-	totsum = -maxint - 1
-	subsum = 0
+	for x in a:
+		maxval = max(maxval, x)
+		minval = min(minval, x)
+	
+	if maxval <= 0: # will have max as neg val so have to print -ve maxval  in this case.
+		return maxval #u can return minval or int 0 its ur choice
+	
+	for i in range(0, size):  # this loop goes for +ve maxval
+		cursum += a[i]
+		maxsum = max(maxsum, cursum)
+		if cursum < 0:
+			cursum = 0
+		
+	return maxsum	
 
-	for i in range(0, size):
-		totsum += a[i]
-		if (subsum < totsum):
-			subsum = totsum
-
-		if totsum < 0:
-			totsum = 0
-
-	return subsum
-
-a = [-2, -3, 4, -1, -2, 1, 5, -3]
+a = [-1, 0, -3, 0]
 
 print (f"total sum = {sum(a)}, max sum of subarray = {maxSubArraySum(a, len(a))}" )
