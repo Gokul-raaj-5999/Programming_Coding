93c6cd66db19bee09d746ebe2f32c75987e698fb
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-09-29 22:00:44 +0530

    cf

---
+++ Codeforce_Coding/368B - Sereja and Suffixes.py
@@ -0,0 +1,64 @@
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+b = set()
+for i in range(0, n):
+    b.add(a[n-i-1])
+    a[n-i-1] = len(b)
+for i in range(0, q):
+    print(a[int(input())-1])
+
+#tle code
+"""n, q = map(int, input().split())
+a = list(map(int, input().split()))
+slst = []
+for i in range(0, n):
+    slst.append(len(set(a[i:])))
+# print(slst)
+for i in range(0, q):
+    print(slst[int(input())-1])"""
+
+
+"""
+B. Sereja and Suffixes
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Sereja has an array a, consisting of n integers a1, a2, ..., an. The boy cannot sit and do nothing, he decided to study an array. Sereja took a piece of paper and wrote out m integers l1, l2, ..., lm (1 ≤ li ≤ n). For each number li he wants to know how many distinct numbers are staying on the positions li, li + 1, ..., n. Formally, he want to find the number of distinct numbers among ali, ali + 1, ..., an.?
+
+Sereja wrote out the necessary array elements but the array was so large and the boy was so pressed for time. Help him, find the answer for the described question for each li.
+
+Input
+The first line contains two integers n and m (1 ≤ n, m ≤ 105). The second line contains n integers a1, a2, ..., an (1 ≤ ai ≤ 105) — the array elements.
+
+Next m lines contain integers l1, l2, ..., lm. The i-th line contains integer li (1 ≤ li ≤ n).
+
+Output
+Print m lines — on the i-th line print the answer to the number li.
+
+Examples
+inputCopy
+10 10
+1 2 3 4 1 2 3 4 100000 99999
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+outputCopy
+6
+6
+6
+6
+6
+5
+4
+3
+2
+1
+"""
