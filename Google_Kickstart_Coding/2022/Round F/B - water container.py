a584a88de5214699b682b2b90d139cfb46b3a77b
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-10-02 14:27:58 +0530

    ks

--- Google_Kickstart_Coding/2022/Round F/B - water container.py
+++ Google_Kickstart_Coding/2022/Round F/B - water container.py
@@ -1,13 +1,15 @@
 import collections
-d = dict()
-d[1] = [1]
 
-def add_adj(a,b):
-    d[a]=d.get(a,[]) + [b]
-    d[b]=d.get(b,[]) + [a]
-
-
-def findNodes_in_lev():
+def solve():
+    d = dict()
+    d[1] = [1]
+    n, f = map(int, input().split())
+    for i in range(0, n-1):
+        a, b = map(int, input().split())
+        d[a]=d.get(a,[]) + [b]
+        d[b]=d.get(b,[]) + [a]
+    
+    # print(d)
     vi=set()
     lans=[]
     q=collections.deque()
@@ -21,29 +23,20 @@ def findNodes_in_lev():
                 if j not in vi:
                     q.append(j)
         lans.append(l)
-    return lans
-
-if __name__ == '__main__':
-    t = int(input())
-    for t in range(0, t):
-        n, f = map(int, input().split())
-        for i in range(0, n-1):
-            a, b = map(int, input().split())
-            add_adj(a,b)
         
-        # print(d)
+    ans=0
+    aqe = []
+    for i in range(0,f):
+        aqe.append(int(input()))
+    for i in lans:
+        if i<=f:
+            ans+=i
+            f-=i
+        else:
+            break
+    return ans
 
-        lans = findNodes_in_lev()
-        # print('nodes in each level',lans)
-        ans=0
-        aqe = []
-        for i in range(0,f):
-            aqe.append(int(input()))
-        for i in lans:
-            if i<=f:
-                ans+=i
-                f-=i
-            else:
-                break
-        print(f"Case #{t+1}: {ans}")
+
+for t in range(0, int(input())):
+    print(f"Case #{t+1}: {solve()}")
     
