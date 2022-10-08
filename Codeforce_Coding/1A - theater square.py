a51035d71a9b8407c4d490b9c9e6e957adc5c7e5
Gokul raaj Velu <gk14.9.1999@gmail.com>
2022-10-08 12:09:58 +0530

    cf

---
+++ Codeforce_Coding/1A - theater square.py
@@ -0,0 +1,33 @@
+import math 
+n,m,a = list(map(int, input().split()))
+count1 = 0
+count2 = 0
+
+count1 = math.ceil(m/a)
+count2 = math.ceil(n/a)
+
+print(count2*count1)
+    
+
+"""
+A. Theatre Square
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a.
+
+What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
+
+Input
+The input contains three positive integer numbers in the first line: n,  m and a (1 ≤  n, m, a ≤ 109).
+
+Output
+Write the needed number of flagstones.
+
+Examples
+inputCopy
+6 6 4
+outputCopy
+4
+"""
