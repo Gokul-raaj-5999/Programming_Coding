d763b819224c7f07d58ec489ef32c2fe9d6476bc
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-10-21 13:10:40 +0530

    Create 125. Valid Palindrome.py

---
+++ Leetcode_Coding/125. Valid Palindrome.py
@@ -0,0 +1,54 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = ''
+        
+        for i in range(0, len(s)):
+            if ((s[i]).lower()).isalnum() :
+                n += (s[i]).lower()
+        print(n)
+        if n == n[::-1]:
+            return True
+        else:
+            return False
+        
+
+"""
+125. Valid Palindrome
+Easy
+
+5107
+
+6154
+
+Add to List
+
+Share
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
