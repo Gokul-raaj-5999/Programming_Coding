1f4abc0b9734dc9e744400b8fa8eb454ea62d498
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-10-31 10:02:55 +0530

    Create 766. Toeplitz Matrix.py

---
+++ Leetcode_Coding/766. Toeplitz Matrix.py
@@ -0,0 +1,69 @@
+class Solution:
+    def isToeplitzMatrix(self, mat: List[List[int]]) -> bool:
+        n = len(mat)
+        m = len(mat[0])
+        a = mat[0]
+        
+        for i in range(0, n-1):
+            tem = mat[i][:-1]
+            if i == n-1:
+                a = mat[i+1]
+            else:
+                a = mat[i+1][1:]
+            
+            print(tem, a)
+            if tem != a:
+                return False
+        else:
+            return True
+            
+        
+
+"""
+766. Toeplitz Matrix
+Easy
+
+2346
+
+131
+
+Add to List
+
+Share
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99
+ 
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+"""
