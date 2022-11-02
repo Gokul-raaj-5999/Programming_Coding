01a4e86e592986c7451ec35858eecae9b60d7743
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-11-02 08:40:47 +0530

    DSA

---
+++ My_personal_Coding/DataStructures/DSA_Queue_usingLinkedList.py
@@ -0,0 +1,50 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue():
+    def __init__(self):
+        self.head = None
+
+    def pushbach(self,data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            return
+        tem = self.head
+        while tem.next:
+            tem = tem.next
+        tem.next = newNode
+        return
+
+    def popfrient(self):
+        if self.head is None:
+            print('empty')
+            return
+        tem = self.head
+        self.head = tem.next
+        return
+
+    def printing(self):
+        if self.head is None:
+            print('empty')
+            return
+        tem = self.head
+        print('printint-> ', end= ' ')
+        while tem.next:
+            print(tem.data , end= ' ')
+            tem = tem.next
+        print(tem.data)
+
+
+
+q = Queue()
+q.pushbach(1)
+q.pushbach(2)
+q.pushbach(3)
+q.printing()
+q.popfrient()
+q.printing()
+q.pushbach(10)
+q.printing()
