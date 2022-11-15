47928e73550046c3dc81282475ecabc9b0189f72
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-11-15 22:49:44 +0530

    Create 50. Pow(x, n).py

---
+++ Leetcode_Coding/50. Pow(x, n).py
@@ -0,0 +1,42 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        x, n = float(x), float(n)
+        ans = x**n
+        a = ("%.6f" % ans)
+        print(a)
+        a = float(str(a)[:-1])
+        return float(a)
+
+
+"""
+50. Pow(x, n)
+Medium
+6.1K
+6.6K
+Companies
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+-104 <= xn <= 104
+"""
