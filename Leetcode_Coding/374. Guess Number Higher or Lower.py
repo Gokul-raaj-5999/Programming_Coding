f87227c88f28e751f2cb0a5a6f6b0f111d762bed
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-11-16 09:30:57 +0530

    Create 374. Guess Number Higher or Lower.py

---
+++ Leetcode_Coding/374. Guess Number Higher or Lower.py
@@ -0,0 +1,64 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        s = 1
+        e = n
+        
+        while True:
+            num = (s+e)//2 
+            x = guess(num)
+            if x == 0:
+                return num
+            elif x == -1:
+                e = num-1
+            else:
+                s = num+1
+
+
+
+"""
+374. Guess Number Higher or Lower
+Easy
+1.9K
+249
+Companies
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+ 
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+"""
