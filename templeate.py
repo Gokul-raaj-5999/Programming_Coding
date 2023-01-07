062554958bad8c409b3be4a428ede928e1255dd7
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2023-01-07 15:34:25 +0530

    tem

--- templeate.py
+++ templeate.py
@@ -1,18 +1,21 @@
 from math import ceil, floor, gcd, lcm
 from bisect import bisect, bisect_left, bisect_right
 
-def func():
-    return  
+def isinteger(x):
+    if floor(x) == ceil(x):
+        return True
+    return False
 
+#----------------------code---------------------------#
 def solve():
+    
 
-    return 
+    return 0
      
 
 for t in range(0, int(input())):
-    # print(solve()) #single varialbe return 
-
-    solve() 
+    print(solve()) #single varialbe return 
+    # solve() 
 
     # arr = solve()   # array return 
     # print(' '.join(str(x) for x in arr))
